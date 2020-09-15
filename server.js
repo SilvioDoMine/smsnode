c25bef5c63d1509a345d4bf6be37f32c2fc48c86
Arthur Henrique <arthurhenrique@live.com>
2020-09-15 09:47:58 -0300

    sendSmS

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const { spawn } = require("child_process");
+const express = require('express')
+const app = express()
 
-const ls = spawn("ls", ["-la"]);
 
-ls.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-});
+app.get('/sms/:number/:text', (req, res) => {
+    const numberToSend = req.params.number.replace(/[^0-9]/g, '')
+    const textToSend = req.params.text
 
-ls.stderr.on("data", data => {
-    console.log(`stderr: ${data}`);
-});
+    const command = spawn(`termux-sms-send -n 015${numberToSend} "${textToSend}"`)
 
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-});
+    command.stdout.on("data", data => {
+        console.log(`stdout: ${data}`);
+    });
+    
+    command.stderr.on("data", data => {
+        console.log(`stderr: ${data}`);
+    });
+    
+    command.on('error', (error) => {
+        console.log(`error: ${error.message}`);
+    });
+    
+    command.on("close", code => {
+        console.log(`child process exited with code ${code}`);
+    });
 
-ls.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-});
+    return true;
+})
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado na porta 3000");
+})
